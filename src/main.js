5412799a1bf450568ed86052153f0b8f4921105a
Lorin Beer <lorin.beer@gmail.com>
2014-07-28 12:15:08 -0700

    cordova initializes a accelerometer event listener

--- src/main.js
+++ src/main.js
@@ -1,9 +1,32 @@
+
+
+
+
 define(function(require, exports, module) {
     // import dependencies
     var Engine = require('famous/core/Engine');
     var Modifier = require('famous/core/Modifier');
     var Transform = require('famous/core/Transform');
     var ImageSurface = require('famous/surfaces/ImageSurface');
+
+    var cdvinit = function() {
+        console.log("CORDOVA INIT FUNCTION");
+        document.addEventListener('deviceready', appinit, false); 
+    };
+
+    var appinit = function() {
+        console.log("APPINIT: watching acceleration");
+        navigator.watchAcceleration(excel,noxel,100);
+    }
+
+    var excel = function (acceleration) {
+        console.log(acceleration);
+    };
+
+    var nocel = function (error) {
+        console.log("ERROR!: ", error);
+    };
+
 
     // create the main context
     var mainContext = Engine.createContext();
