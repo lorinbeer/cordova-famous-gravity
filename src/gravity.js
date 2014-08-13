d026647e4f8753ec8b4b36f74878390754c24243
Lorin Beer <lorin.beer@gmail.com>
2014-08-13 12:40:09 -0700

    demo code cleanup

--- src/gravity.js
+++ src/gravity.js
@@ -12,13 +12,16 @@ var app = {
 
         var mainContext,
             logo,
-            centerSpinModifier,
+            spinMod,
+            dropMod,
+            spring,
             time;
 
-        Transitionable.registerMethod('spring', SpringTransition);
- 
+
+        Transitionable.registerMethod('spring', SpringTransition); 
         mainContext = Engine.createContext();
         
+
         logo = new ImageSurface({
             size: [200, 200],
             content: 'famous_logo.svg',
@@ -42,26 +45,13 @@ var app = {
 
         dropMod = new Modifier({
             origin: [0.5,0.0],
-            transform : function() {
-                return Transform.translate()
-            }
         });
- 
-        var springDropMod = new Modifier({origin:[0.5,0.0]});
-
-  var stateModifier = new StateModifier({
-  origin: [0.5, 0]
-});     
-
-//        logo.chain.addModifier(spinMod, springDropMod)
-        mainContext.add(stateModifier).add(logo);
-        //mainContext. 
        
-stateModifier.setTransform(
-  Transform.translate(0, 300, 0), spring
-);
- 
-        springDropMod.setTransform(Transform.translate(0,300,0), spring);
+        dropMod.setTransform(
+            Transform.translate(0, 300, 0), spring
+        );
+        
+        mainContext.add(dropMod).add(spinMod).add(logo);
     }
 };
 
