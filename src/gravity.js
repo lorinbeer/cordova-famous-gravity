c0a8f3b0d53e1f150bbf1f276ea6bc312cb2addc
Lorin Beer <lorin.beer@gmail.com>
2014-08-12 23:26:28 -0700

    removed dead code

--- src/gravity.js
+++ src/gravity.js
@@ -24,26 +24,3 @@ var app = {
         mainContext.add(centerSpinModifier).add(logo);
     }
 };
-
-
-/*
-    // create the main context
-    var mainContext = Engine.createContext();
-
-    // your app here
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: 'http://code.famo.us/assets/famous_logo.svg',
-        classes: ['double-sided']
-    });
-
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function(){
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
-
-    mainContext.add(centerSpinModifier).add(logo);
-*/
