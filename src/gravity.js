1ad929547bca15384085fda50aa86c52ad675d3b
Lorin Beer <lorin.beer@gmail.com>
2014-08-13 12:03:20 -0700

    referenced appropriate famous classes, to create spring drop

--- src/gravity.js
+++ src/gravity.js
@@ -3,13 +3,20 @@ var app = {
         var Engine = famous.core.Engine,
             Modifier = famous.core.Modifier,
             Transform = famous.core.Transform,
-            ImageSurface = famous.surfaces.ImageSurface;
+            Surface = famous.core.Surface,
+            ImageSurface = famous.surfaces.ImageSurface,
+            Transitionable = famous.transitions.Transitionable,
+            StateModifier = famous.modifiers.StateModifier,
+            SpringTransition = famous.transitions.SpringTransition,
+            ModifierChain = famous.modifiers.ModifierChain;
 
         var mainContext,
             logo,
             centerSpinModifier,
             time;
 
+        Transitionable.registerMethod('spring', SpringTransition);
+ 
         mainContext = Engine.createContext();
         
         logo = new ImageSurface({
@@ -20,15 +27,41 @@ var app = {
 
         time = Date.now();
 
-        centerSpinModifier = new Modifier({
+        var spring = {
+            method: 'spring',
+            period: 1000,
+            dampingRatio: 0.3
+        };
+
+        spinMod = new Modifier({
             origin: [0.5,0.0],
             transform : function(){
                 return Transform.rotateY(.002 * (Date.now() - time));
             }
         });
 
+        dropMod = new Modifier({
+            origin: [0.5,0.0],
+            transform : function() {
+                return Transform.translate()
+            }
+        });
+ 
+        var springDropMod = new Modifier({origin:[0.5,0.0]});
 
+  var stateModifier = new StateModifier({
+  origin: [0.5, 0]
+});     
 
-        mainContext.add(centerSpinModifier).add(logo);
+//        logo.chain.addModifier(spinMod, springDropMod)
+        mainContext.add(stateModifier).add(logo);
+        //mainContext. 
+       
+stateModifier.setTransform(
+  Transform.translate(0, 300, 0), spring
+);
+ 
+        springDropMod.setTransform(Transform.translate(0,300,0), spring);
     }
 };
+
